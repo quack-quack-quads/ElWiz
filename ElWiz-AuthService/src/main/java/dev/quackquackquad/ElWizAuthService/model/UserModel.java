c53481498a8b71db9cd7bef8de8f56b44491cf4f
RohitShah1706 <rlshah03@gmail.com>
2023-06-29 21:14:04 +0530

    updated db to postgresql - next step: updated workflows

--- ElWiz-AuthService/src/main/java/dev/quackquackquad/ElWizAuthService/model/UserModel.java
+++ ElWiz-AuthService/src/main/java/dev/quackquackquad/ElWizAuthService/model/UserModel.java
@@ -1,11 +1,8 @@
 package dev.quackquackquad.ElWizAuthService.model;
-import dev.quackquackquad.ElWizAuthService.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
