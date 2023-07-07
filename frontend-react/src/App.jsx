520516463ea3237ab5a0028a18e97eb611072d44
Heliospook <subhajeetlahiri@gmail.com>
2023-07-08 00:32:42 +0530

    login page built

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import './App.scss'
 import { BrowserRouter as Router, Routes, Route}from 'react-router-dom'
+
 import Lander from './screens/Lander/Lander'
+import Login from './screens/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Lander/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </Router>
   </div>
