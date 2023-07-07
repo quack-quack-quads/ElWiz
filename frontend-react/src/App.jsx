d4a89f65e098e6aa163a3c23e989e5c6a20648df
Heliospook <subhajeetlahiri@gmail.com>
2023-07-08 01:42:30 +0530

    signup page built

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route}from 'react-router-dom'
 
 import Lander from './screens/Lander/Lander'
 import Login from './screens/Login/Login'
+import Signup from './screens/Signup/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,8 +12,9 @@ function App() {
   return <div className="App">
     <Router>
       <Routes>
-        <Route path='/' element={<Lander/>}/>
+        <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Lander/>}/>
       </Routes>
     </Router>
   </div>
